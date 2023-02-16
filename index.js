04984fac9c62669a2f4be93ffdc22164e2375937
alvaro <alvaroawake@gmail.com>
2023-02-15 23:51:28 -0400

    fix general route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-require('dotenv').config();
-const cors =  require('cors')
-const { dbConnection } = require('./database/config');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { dbConnection } = require("./database/config");
 
 // console.log(process.env)
 
@@ -10,23 +10,26 @@ const app = express();
 //Base de datos
 dbConnection();
 
-app.use(cors())
+app.use(cors());
 
 //Directorio publico
 //use es un middleware
 app.use(express.static("public"));
 
 //Lectura y parseo del body
-app.use(express.json())
-
+app.use(express.json());
 
 //Rutas
 //auth // crear, login , renew
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/events', require('./routes/events'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/events", require("./routes/events"));
+
+app.get("*", (req, req) => {
+	res.sendFile(__dirname + "/public/index.html");
+});
 //CRUD: Eventos
 
 //escuchar peticiones
 app.listen(process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
-})
+	console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
